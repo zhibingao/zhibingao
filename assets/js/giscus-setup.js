2c17c8ead05f14cd938575277a235bea1e66b4b4
zhibingao <zhibingao@outlook.com>
2026-02-24 17:28:05 +0000

    Deploying to gh-pages from @ zhibingao/zhibingao.github.io@ef2c8af761facac94def3badec82570fdece9fd7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zhibingao/zhibingao",
+    "data-repo": "zhibingao/zhibingao.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
